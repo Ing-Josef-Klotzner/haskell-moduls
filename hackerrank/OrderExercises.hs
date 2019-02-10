dab15a323de625f0415b01174f62d3f44076fea2
Ing-Josef-Klotzner <josef.klotzner@gmail.com>
2019-02-11 00:07:28 +0100

    solved OrderExercises

--- hackerrank/OrderExercises.hs
+++ hackerrank/OrderExercises.hs
@@ -26,7 +26,7 @@ findBgst inputV = go 0 0 0 0 1 where
 -- faster: Kadane algorithm O (n)
 -- find maximum sum of subarray, return (sum, startindex, length)
 kadane :: V.Vector Int -> (Int, Int, Int)
-kadane inputV = go 0 sm1 sm1 0 0 1 where
+kadane inputV = go 0 sm1 sm1 0 1 1 where
     sm1 = inputV V.! 0
     go simOld sm meh sim lgm si
         | si == len = (sm, simOld, lgm)
@@ -35,9 +35,9 @@ kadane inputV = go 0 sm1 sm1 0 0 1 where
         simON = if newMax then sim else simOld
         simN = if newStart then si + 1 else sim
         lgmN = if newMax then si - simON + 1 else lgm
-        newStart = mehN < 0
-        newMax = smN /= sm
         currNr = inputV V.! si
+        newStart = mehN < 0
+        newMax = smN > sm
         smN = max sm mehN
         mehN = max currNr (meh + currNr)
         len = V.length inputV
@@ -53,19 +53,18 @@ kadaneL inputV k = go sm1 sm1 1 V.empty where
         kadLN = if newStart && sm > 0 then V.snoc kadL sm else kadL
         -- max until next newStartg (mehN < 0) - is there a bigger number behind?
         -- done with a sub kadane search
-        maxNnewStart = gog 0 meh (si - 0)
+        maxNnewStart = gog 0 meh si
             where
             gog smg mehg sig
-                | sig == len || newStartg = smg -- error (show smg ++ " " ++ show sig) --smg
+                | sig == len || newStartg = smg
                 | True = gog smgN mehgN (sig + 1)
                 where
                 currNrg = inputV V.! sig
                 newStartg = mehgN < 0
                 smgN = max smg mehgN
                 mehgN = max currNrg (mehg + currNrg)
-        newStart = mehN < 0 || newDsjnt
+        newStart = mehN <= 0 && meh >= maxNnewStart || newDsjnt
         newDsjnt = if mehN < meh && mehN > 0 then meh >= maxNnewStart else False
---        newMax = smN > sm
         currNr = inputV V.! si
         smN = if newStart then 0 else max sm mehN
         mehNN = if newDsjnt then 0 else mehN
@@ -77,8 +76,19 @@ kadaneL inputV k = go sm1 sm1 1 V.empty where
         max_ending_here = max(x, max_ending_here + x)
         max_so_far = max(max_so_far, max_ending_here)   sm
     return max_so_far -}
-shorten :: V.Vector Int -> Int -> Int -> V.Vector Int
-shorten inputV si lg = (V.slice 0 si inputV) V.++ (V.slice (si + lg) (V.length inputV - si - lg) inputV)
+shorten :: V.Vector Int -> Int -> Int -> Int -> V.Vector Int
+shorten inputV si lg sm = lftPrt V.++ rgtPrt
+    where
+    lftPrt
+        | isInit = V.empty
+        | True = lftSlc
+    rgtPrt
+        | isLast = V.empty
+        | True = rgtSlc
+    lftSlc = V.slice 0 si inputV
+    rgtSlc = V.slice (si + lg) (V.length inputV - si - lg) inputV
+    isInit = V.sum lftSlc == sm
+    isLast = V.sum rgtSlc == sm  
 
 -- O (n² * k) 
 bgstL :: V.Vector Int -> Int -> [Int]
@@ -89,18 +99,30 @@ bgstL inputV k = go [] inputV k where
         where
         bLstN = bLst ++ [sm]
         (sm, si, lg) = findBgst inputV_
-        sInputV = shorten inputV_ si lg
+        sInputV = shorten inputV_ si lg sm
 
 -- O (n * k)
 bgstLKadane :: V.Vector Int -> Int -> [Int]
-bgstLKadane inputV k = go [] inputV k where
+bgstLKadane inputV k = go V.empty inputV k where
     go bLst inputV_ k_
         | k_ /= 0 && bLstN /= bLst && sm > 0 = go bLstN sInputV (k_ - 1)
-        | True = bLst
+        | True = V.toList bLst
         where
-        bLstN = bLst ++ [sm]
+        bLstN = V.snoc bLst sm
         (sm, si, lg) = kadane inputV_
-        sInputV = shorten inputV_ si lg
+        sInputV = shorten inputV_ si lg sm
+
+---- changed for debug
+---- O (n * k)
+----bgstLKadane :: V.Vector Int -> Int -> [Int]
+--bgstLKadane inputV k = go V.empty inputV k where
+--    go bLst inputV_ k_
+--        | k_ /= 0 && bLstN /= bLst && sm > 0 = go bLstN sInputV (k_ - 1)  -- > 0
+--        | True = V.toList inputV_
+--        where
+--        bLstN = V.snoc bLst sm
+--        (sm, si, lg) = kadane inputV_
+--        sInputV = shorten inputV_ si lg sm
 
 main :: IO ()
 main = do
@@ -113,6 +135,10 @@ main = do
 
     putStr $ unlines $ map show $ kadaneL inputV k
 --    putStr $ unlines $ map show $ bgstLKadane inputV k
+
+-- changed for debug:
+--    putStr $ unwords $ map show $ bgstLKadane inputV k
+
 --    putStr $ unlines $ map show $ bgstL inputV k
 {-
 kadane $ V.fromList [9404,8036,-9334,-9146,8085,3024,988,5875,2264,-4643,-8916,-8072,1954,3424,5364,-2633,-8910,-7310,9443,-5096,4982,-7834,5164,-8360,185,265,277,-4154,-6615,6233,5988,-9008,5849,-948,6458,-9633,7955,432,1308,6533,-4667,9545,9446,1002,4452,-2285,-2413,-8734,4224,5492,-6250,-38,-3089,-6761,2326,-2209,-7962,-929,5710,-391,-6415,5399,4758,933,-3318,-8572,566,8181,-1512,-2937,-5897,5525,-7054,912,-8863,-4893,2963,-8827,-8376,5579,-8906,2265,5349,9388,4664,5708,2630,-4177,7665,6774,-5152,-5504,6138,2018,2464,3936,-5985,9804,-9520,1245]
